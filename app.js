0b089468489ed1be9dedf83c0f2e449f7c95524c
guiparpineli <guiparpineli@hotmail.com>
2021-10-26 22:21:41 -0300

    to perdido

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 Testando erro de conflito do 
 git, vamos ver se 
 bugamos tudo e arrumamos, 
 show de bola
-=======
-Olá aqui é a Luiza, tudo bem? 
->>>>>>> 9e4fb3d4412f10216d19df1a95ce89c86872b3b5
